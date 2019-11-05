a6c9e31a1c199c3b4103692c671e7661655b38c4
Gigi <dergigi@pm.me>
2019-11-05 22:52:04 +0100

    Verbose delete

--- clone-readability.sh
+++ clone-readability.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rfv readability
 git clone https://github.com/mozilla/readability.git
-rm -rf readability/.git
+rm -rfv readability/.git
 git add readability
 git commit -m "Update readability from github"
