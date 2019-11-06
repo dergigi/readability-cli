2aa38680e6a3925fb1e3fdbfbe8da86e98b07e57
Gigi <dergigi@pm.me>
2019-11-06 14:22:19 +0100

    Remove console log of url

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var parseremote = require('./parseremote');
 program
 .arguments('<url>')
 .action(function(url) {
-  console.log('url: %s', url);
   let result  = parseremote.parseUrl(url)
   console.log(result)
 })
